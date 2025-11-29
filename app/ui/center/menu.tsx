40c028bdd3a0a9090a7e02f7e35b4a27ec026112
EliasRivamar <eliasrivamar11@gmail.com>
2025-11-29 18:29:20 -0300

    chore: fixing

--- app/ui/center/menu.tsx
+++ app/ui/center/menu.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { signOutAction } from '@/app/lib/actions';
-import { signOut } from '@/app/pages/api/auth/[...nextauth]';
+import { signOut } from '@/app/api/auth/[...nextauth]/route';
 import Link from 'next/link';
 import styles from './menu.module.css';
 import { useState } from "react";
